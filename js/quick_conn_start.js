314be0fb8422caa6a8af07909ff04ebbcb74280d
niknah <git@mail.niknah.com>
2024-08-21 13:40:34 +1000

    Fix combo settings needs parseInt because it is a string

--- js/quick_conn_start.js
+++ js/quick_conn_start.js
@@ -1,8 +1,10 @@
+/* eslint quotes:0 */
+/* eslint prefer-spread:0 */
 
+// eslint-disable-next-line import/no-unresolved
 import { app } from "../../scripts/app.js";
 import { QuickConnection } from "./QuickConnection.js";
 import { CircuitBoardLines } from './CircuitBoardLines.js';
-
 
 const quickConnection = new QuickConnection();
 quickConnection.init();
@@ -18,7 +20,7 @@ const ext = {
 		circuitBoardLines.initOverrides(app.canvas);
 
 		const quickConnectionId = "quick-connections.enable";
-    app.ui.settings.addSetting({
+		app.ui.settings.addSetting({
 			id: quickConnectionId,
 			name: "Quick connections enable",
 			type: "boolean",
@@ -26,33 +28,30 @@ const ext = {
 			onChange: (...args) => {
 				quickConnection.enabled = app.ui.settings.getSettingValue(quickConnectionId, true);
 				return app.graph.change.apply(app.graph, args);
-			}
-    });
+			},
+		});
 
 		const circuitBoardId = "circuit-board-lines.enable";
-    app.ui.settings.addSetting({
+		app.ui.settings.addSetting({
 			id: circuitBoardId,
 			name: "Circuit Board lines",
 			type: "combo",
-			defaultValue: true,
 			options: [
-				{value:0, text:"Off"},
-				{value:1, text:"Circuit board"},
+				{ value: 0, text: "Off" },
+				{ value: 1, text: "Circuit board" },
 				// On top doesn't place the wires on top of the text boxes
-				// {value:2, text:"On top"},
+				{ value: 2, text: "On top" },
 			],
 			defaultValue: 1,
 
 			onChange: (...args) => {
-				const option = app.ui.settings.getSettingValue(circuitBoardId, 1);
+				const option = parseInt(app.ui.settings.getSettingValue(circuitBoardId, 1), 10);
 				circuitBoardLines.enabled = (option === 1);
-				// app.graph.config.links_ontop = (option === 2);
+				app.graph.config.links_ontop = (option === 2);
 				return app.graph.change.apply(app.graph, args);
-			}
-    });
-	}
+			},
+		});
+	},
 };
 
 app.registerExtension(ext);
-
-
