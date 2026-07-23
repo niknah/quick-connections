595972b23e7106dc55fd186892a3bcf9a600557c
niknah <github@mail.niknah.com>
2026-07-23 18:56:30 +1000

    Added icon

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+	{
+		files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+		rules: {
+		},
+	},
+]);
